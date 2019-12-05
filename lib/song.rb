b89c65f2c294a857327516cb8eca137fb7739ed0
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-05 00:04:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,9 +17,15 @@ class Song
   end
 
   def artist_name=(name)
+    found = false
     Artist.all.each do |singer|
       if singer.name == name
         self.artist = singer
+        found = true
+      end
+      if !found
+        guy = Artist.new(name)
+        self.artist = guy
       end
     end
     self.artist.name
