a2790fb4b5d6fe74df91e13ecc8b2330c3db5458
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-05 02:01:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,9 +14,7 @@ class Song
   def self.new_by_filename(filename)
     song_info = filename.split(" - ")
     song = Song.new(song_info[1])
-    artist = Artist.find_or_create_by_name(song_info[0])
     puts song
-    song.artist = artist
   end
 
   def artist_name=(name)
