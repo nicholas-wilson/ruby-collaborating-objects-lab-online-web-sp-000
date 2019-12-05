b54ba19f3a23aac6d6f089f4e2c0d556611994c8
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-05 01:19:28 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,6 @@ class MP3Importer
   def files
     @files = Dir["#{@path}**/*.mp3"]
     @files.each do |file|
-      puts (File.basename(file))
       @files << File.basename("#{file}.mp3")
     end
   end
